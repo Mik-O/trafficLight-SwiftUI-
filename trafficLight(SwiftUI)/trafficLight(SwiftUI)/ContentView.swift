1cdf025161bf50ff8474f15dbfafa37700c96d3b
mikle <mikle.kontinent@gmail.com>
2025-07-24 16:57:45 +0700

    final

--- trafficLight(SwiftUI)/trafficLight(SwiftUI)/ContentView.swift
+++ trafficLight(SwiftUI)/trafficLight(SwiftUI)/ContentView.swift
@@ -13,9 +13,22 @@ enum CurrentLight {
 
 struct ContentView: View {
     
-    @State private var buttonName = "Start"
+    @State private var buttonName = "START"
     
     @State private var currentLight = CurrentLight.off
+    
+    private func nextColor() {
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+        case .yellow:
+            currentLight = .green
+        case .green:
+            currentLight = .red
+        case .off:
+            currentLight = .red
+        }
+    }
     
     
     var body: some View {
@@ -24,7 +37,7 @@ struct ContentView: View {
         ZStack {
             Color(.black)
                 .ignoresSafeArea()
-            VStack {
+            VStack(spacing: 20) {
                 ColorLight(color: .red, opacity: currentLight == .red ? 1 : 0.3)
                 ColorLight(color: .yellow, opacity: currentLight == .yellow ? 1 : 0.3)
                 ColorLight(color: .green, opacity: currentLight == .green ? 1 : 0.3)
@@ -32,18 +45,12 @@ struct ContentView: View {
                 Spacer()
                 
                 ChangeColorButton(
-                    title: "Next",
+                    title: buttonName,
                     action: {
-                        switch currentLight {
-                        case .red:
-                            currentLight = .yellow
-                        case .yellow:
-                            currentLight = .green
-                        case .green:
-                            currentLight = .red
-                        case .off:
-                            currentLight = .red
+                        if buttonName == "START" {
+                            buttonName = "NEXT"
                         }
+                        nextColor()
                     }
                 )
             }
@@ -55,15 +62,10 @@ struct ContentView: View {
 
 
 
-
-
-
-
-
-
 #Preview {
     ContentView()
 }
+
 
 
 
